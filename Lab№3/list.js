3c50348e6e83ad4ba6579cd3d211a1e46113ee9a
Egor2341 <95289626+Egor2341@users.noreply.github.com>
2023-10-16 17:45:09 +0300

    Update list.js

--- Lab№3/list.js
+++ Lab№3/list.js
@@ -4,10 +4,9 @@ function list() {
   
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
         }
